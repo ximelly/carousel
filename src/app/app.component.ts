ac2f7b04c3f06907eb372b3bed4e6e666711f221
Ximelly <ximelly@163.com>
2017-06-27 10:55:15 +0800

    轮播图
    本地代码同步

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component,OnDestroy,AfterViewInit} from '@angular/core';
+import {trigger,state,style,animate,transition} from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,21 +8,56 @@ import { Component } from '@angular/core';
 export class AppComponent{
   imgbegin='assets/images/banner';
   imgend='.jpg';
-  slides:any[]=[];
-  myInterval:number=1500;
-  slideNum:number=0;
-  leftLenth="0";
+  public slides:any[]=[];//图片数组
+  public changeBar=true;//是否显示左右切箭头
+  public myInterval:number=2500;//轮播切换时间
+  public slideNum:number=0;//初始化当前banenr
+  private leftLenth;
+  private timer;
   constructor(){
       for(let i=1;i<4;i++){
         this.slides.push(this.imgbegin+i+this.imgend);
       }
-      setInterval(() => {
-            this.leftLenth=(-this.slideNum*100).toString()+"%";
-            if(this.slideNum>=2){
-              this.slideNum=0;
-            }else{
-              this.slideNum++;
-            }
-        },this.myInterval);
+      this.startScroll();
+  }
+  startScroll(){
+      this.timer=setInterval(() => {
+          this.scrollRight();
+      },this.myInterval);
+  }
+  circleClick(i){
+    this.slideNum=i;
+    this.leftLenth=(-this.slideNum*100).toString()+"%";
+  }
+  leftClick(){
+    this.scrollLeftt();
+  }
+  rightClick(){
+    this.scrollRight();
+  }
+  scrollRight(){
+    if(this.slideNum>=2){
+      this.slideNum=0;
+    }else{
+      this.slideNum++;
+    }
+    this.leftLenth=(-this.slideNum*100).toString()+"%";
+  }
+
+  scrollLeftt(){
+    if(this.slideNum<=0){
+      this.slideNum=2;
+    }else{
+      this.slideNum--;
+    }
+    this.leftLenth=(-this.slideNum*100).toString()+"%";
+  }
+  onmouseenter(){
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+  onmouseleave(){
+    this.startScroll();
   }
 }
